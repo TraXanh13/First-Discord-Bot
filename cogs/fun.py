21968fa4d097a929f5ef09420103c524315bc1f4
Kyle <kyleduong13@gmail.com>
2021-01-03 18:00:19 -0700

    First Commit

---
+++ cogs/fun.py
@@ -0,0 +1,53 @@
+# import discord
+import random
+from discord.ext import commands
+from main import display_embed
+
+
+class Fun(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(aliases=["depression", "depressed", "sadened"])
+    async def sad(self, ctx):
+        response = ["Good", "Feels bad lmao", "kek", "Oof", "Sucks to suck", "R U HAVE DUMB?"]
+        await display_embed(ctx, "You sad?", (random.choice(response)))
+
+    @commands.command(aliases=["8ball"])
+    async def eightball(self, ctx, *, question):
+        response = ["As I see it, yes", "Ask again later", "Better not tell you now", "Cannot predict now",
+                    "Concentrate and ask again", "Don’t count on it", "It is certain", "It is decidedly so",
+                    "Most likely", "My reply is no", "My sources say no", "Outlook good", "Outlook not so good",
+                    "Reply hazy try again", "Signs point to yes", "Very doubtful", "Without a doubt", "Yes",
+                    "Yes, definitely", "You may rely on it"]
+        await display_embed(ctx, "The Magic 8 Ball", f"Question: {question}\nResponse: {random.choice(response)}")
+
+    @commands.command()
+    async def flip(self, ctx, flips=1):
+        heads = 0
+        tails = 0
+        winner = ""
+
+        if flips > 10:
+            flips = 10
+
+        for x in range(flips):
+            flip_res = random.randint(0, 1)
+            if flip_res == 0:
+                heads += 1
+            else:
+                tails += 1
+
+        if heads > tails:
+            winner = f"Heads Wins!\nHeads: {heads}\nTails: {tails}"
+        elif heads < tails:
+            winner = f"Tails Wins!\nHeads: {heads}\nTails: {tails}"
+        else:
+            winner = f"Its a tie!\nHeads: {heads}\nTails: {tails}"
+
+        await display_embed(ctx, "Coin Flip", winner)
+
+
+def setup(client):
+    client.add_cog(Fun(client))
---
+++ cogs/points.py
@@ -0,0 +1,68 @@
+import discord
+import json
+from discord.ext import commands
+from main import display_embed
+
+
+# Specific user permission check
+def user_check(ctx):
+    return ctx.author.id == 167416077322682368
+
+
+class Points(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    @commands.check(user_check)
+    async def give_points(self, ctx, member: discord.Member, points=1):
+        with open("./text/points.json", 'r') as f:
+            point_list = json.load(f)
+
+        if member.mention in point_list:
+            temp = int(point_list[str(member.mention)])
+            point_list[str(member.mention)] = str(temp + points)
+        else:
+            point_list[str(member.mention)] = str(points)
+
+        with open("./text/points.json", 'w') as f:
+            json.dump(point_list, f, indent=4)
+
+        await display_embed(ctx, "Points Awarded", f"{points} awarded to {member.display_name}")
+
+    @commands.command()
+    @commands.check(user_check)
+    async def take_points(self, ctx, member: discord.Member, points=1):
+        with open("./text/points.json", 'r') as f:
+            point_list = json.load(f)
+
+        if member.mention not in point_list:
+            await display_embed(ctx, "Points Taken", f"{member.display_name} isn't on the list")
+            return
+
+        temp = int(point_list[str(member.mention)])
+        if temp < points:
+            point_list[str(member.mention)] = "0"
+        else:
+            point_list[str(member.mention)] = str(temp-points)
+
+        with open("./text/points.json", 'w') as f:
+            json.dump(point_list, f, indent=4)
+
+        await display_embed(ctx, "Points Taken", f"{points} taken from {member.display_name}")
+
+    @commands.command()
+    async def show_points(self, ctx):
+        temp = ""
+        with open("./text/points.json", 'r') as f:
+            lines = f.readlines()
+
+        for i in lines:
+            temp += i
+
+        await display_embed(ctx, "Points List", temp)
+
+
+def setup(client):
+    client.add_cog(Points(client))
